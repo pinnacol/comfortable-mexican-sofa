7c8dc94dddf8fb87bf6210d5ccf7a022af9e931b
DeLynn Berry <delynn@gmail.com>
2011-08-15 22:57:53 -0600

    improve the fixture model by using activemodel.

--- app/models/cms/fixture.rb
+++ app/models/cms/fixture.rb
@@ -1,11 +1,58 @@
 class Cms::Fixture
+  extend ActiveModel::Naming
+  extend ActiveModel::Translation
+  include ActiveModel::Conversion
+  include ActiveModel::Validations
+
+  attr_accessor :name, :path
+  attr_reader   :errors
+
   def self.all
     Pathname.new(ComfortableMexicanSofa.config.fixtures_path).children.collect do |c|
-      new(c)
+      new(:name => c.basename.to_s, :path => c)
     end
   end
 
-  def initialize(path)
+  def self.human_attribute_name(attr, options = {})
+    attr
+  end
 
+  def self.lookup_ancestors
+    [self]
+  end
+
+  def initialize(attributes = {})
+    attributes.each do |name, value|
+      send("#{name}=", value) if respond_to?(name)
+    end
+    @errors = ActiveModel::Errors.new(self)
+  end
+
+  def destroyed?
+    true
+  end
+
+  def new_record?
+    !persisted?
+  end
+
+  def persisted?
+    !path.nil?
+  end
+
+  def read_attribute_for_validation(attr)
+    send(attr)
+  end
+
+  def to_key
+    persisted? ? [name.gsub('.', '-')] : nil
+  end
+
+  def to_s
+    name
+  end
+
+  def valid?
+    true
   end
 end
