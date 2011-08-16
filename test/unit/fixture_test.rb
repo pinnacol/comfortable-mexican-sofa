0e2fdee6e4f0eb22d09b9c04a03461f9adcfcbb9
DeLynn Berry <delynn@gmail.com>
2011-08-15 22:57:21 -0600

    moved the fixture test into the models directory.

--- test/unit/fixture_test.rb
+++
@@ -1,8 +0,0 @@
-require File.expand_path('../test_helper', File.dirname(__FILE__))
-
-class FixtureTest < ActiveSupport::TestCase
-  def test_all
-    assert_respond_to Cms::Fixture, :all
-    assert_kind_of Array, Cms::Fixture.all
-  end
-end
---
+++ test/unit/models/fixture_test.rb
@@ -0,0 +1,14 @@
+require File.expand_path('../../test_helper', File.dirname(__FILE__))
+
+class FixtureTest < ActiveSupport::TestCase
+  include ActiveModel::Lint::Tests
+
+  def setup
+    @model = Cms::Fixture.new
+  end
+
+  def test_all
+    assert_respond_to Cms::Fixture, :all
+    assert_kind_of Array, Cms::Fixture.all
+  end
+end
